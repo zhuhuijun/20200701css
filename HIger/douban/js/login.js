7aac65e43fa05dee2b0b83c47413237290275941
zhuhuijun <zhuhuijun@beisen.com>
2020-11-24 10:49:09 +0800

    sf

--- HIger/douban/js/login.js
+++ HIger/douban/js/login.js
@@ -4,12 +4,17 @@ $(function () {
         console.info('smslogin');
         $('#smsloginform').show();
         $('#pwdloginform').hide();
+        $("#smslogin").removeClass("selected")
+        $("#smslogin").addClass("selected")
+        $("#pwdlogin").removeClass("selected")
     })
     $("#pwdlogin").click(function () {
         console.info('smslogin');
         $('#pwdloginform').show();
         $('#smsloginform').hide();
-
+        $("#pwdlogin").removeClass("selected")
+        $("#pwdlogin").addClass("selected")
+        $("#smslogin").removeClass("selected")
     })
 });
 
